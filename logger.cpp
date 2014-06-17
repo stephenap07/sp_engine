e04d741714e504346e6af2f9a92871e21456eefa
Stephen Pridham <stephenap07@gmail.com>
2014-06-17 09:23:30 -0400

    Move logging into separate module

---
+++ logger.cpp
@@ -0,0 +1,17 @@
+#include <cstdarg>
+#include <cstdio>
+#include "logger.h"
+
+namespace sp {
+namespace log {
+
+void ErrorLog(const char* format, ...)
+{
+    va_list argptr;
+    va_start(argptr, format);
+    vfprintf(stderr, format, argptr);
+    va_end(argptr);
+}
+
+} // namespace log
+} // namespace sp
