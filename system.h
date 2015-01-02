eebdbcdc87ddeeffc188c27c529c154376100c5b
Stephen Pridham <stephenap07@gmail.com>
2015-01-01 23:32:04 -0500

    Fix system information querying

--- system.h
+++ system.h
@@ -5,29 +5,29 @@ namespace sp {
 
 struct SystemInfo
 {
-    void Init()
+    void QuerySystemInformation()
     {
-        strcpy(platform, SDL_GetPlatform());
         num_cpus = SDL_GetCPUCount();
         ram = SDL_GetSystemRAM();
         l1_cache = SDL_GetCPUCacheLineSize();
 
-        const GLubyte *_vendor = glGetString(GL_VENDOR);
-        const GLubyte *_renderer = glGetString(GL_RENDERER);
-        const GLubyte *_version = glGetString(GL_VERSION);
+        const char *_vendor = reinterpret_cast<const char*>(glGetString(GL_VENDOR));
+        const char *_renderer = reinterpret_cast<const char*>(glGetString(GL_RENDERER));
+        const char *_version = reinterpret_cast<const char*>(glGetString(GL_VERSION));
 
-        memcpy(vendor, _vendor, sizeof(_vendor) + 1);
-        memcpy(renderer, _renderer, sizeof(_renderer) + 1);
-        memcpy(version, _version, sizeof(_version) + 1);
+        strcpy(platform, SDL_GetPlatform());
+        strcpy(vendor, _vendor);
+        strcpy(renderer, _renderer);
+        strcpy(version, _version);
     }
 
-    char platform[128];
     int num_cpus;
     int ram;
     int l1_cache;
-    GLubyte vendor[128];
-    GLubyte renderer[128];
-    GLubyte version[128];
+    char platform[128];
+    char vendor[128];
+    char renderer[128];
+    char version[128];
 };
 
 } // namespace sp
